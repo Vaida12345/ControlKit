63a408bcbabb4eabfc485595507c1c0aa902552f
Vaida <developer@vaidaf.net>
2024-06-15 01:47:47 +1000

    updates docs

--- Client/source.swift
+++ Client/source.swift
@@ -8,15 +8,7 @@ public struct MainApp {
     
     public static func main() async throws {
         
-        try await Keyboard.press("`", modifiers: .command)
-        try await Task.sleep(for: .seconds(0.2))
         
-        for _ in 0...100 {
-            let pi = Double.pi.description
-            try await Keyboard.press(pi)
-            try await Keyboard.press(.enter)
-        }
-
     }
     
 }
--- Sources/ControlKit/Screen.swift
+++ Sources/ControlKit/Screen.swift
@@ -10,6 +10,9 @@ import CoreGraphics
 
 public struct Screen {
     
+    /// Captures the current screen.
+    ///
+    /// - Experiment: On a benchmark with `-O`, it takes 50ms on average to capture. That is around 200 frames per second. It took around 8% of the CPU to capture non-stop.
     @inlinable
     public static func capture() -> CGImage? {
         let mainDisplayId = CGMainDisplayID()
